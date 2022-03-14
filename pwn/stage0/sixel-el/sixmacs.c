13a6b1782101ddb01be015d93a0265c0cb3b9fb5
Jakob L. Kreuze <zerodaysfordays@sdf.org>
2022-03-14 19:36:43 -0400

    Initial Emacs container

--- pwn/stage0/sixel-el/sixmacs.c
+++ pwn/stage0/sixel-el/sixmacs.c
@@ -1,9 +1,30 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <emacs-module.h>
-#include <sixel/sixel.h>
+#include <sixel.h>
+
+#include "md5.c"
+
+// ---
+
+typedef struct emacs_value_tag *emacs_value;
+struct emacs_value_tag { void *v; };
+
+// ---
+
+struct hash {
+		uint32_t h0, h1, h2, h3;
+};
+
+#define CACHESZ 8
+static struct hash cache_identifiers[CACHESZ];
+static struct emacs_value_tag *cache[CACHESZ];
+static int inuse[CACHESZ];
+
+// ---
 
 int plugin_is_GPL_compatible;
 
@@ -12,21 +33,20 @@ void panic(emacs_env *ENV, char *message)
 		emacs_value message_object = ENV->make_string(ENV, message, strlen(message));
 		emacs_value symbol = ENV->intern(ENV, "sixel-decode-string");
 		emacs_value args[] = {symbol, message_object};
-		ENV->funcall(ENV, ENV->intern(ENV, "throw"), 2, args);	
+		ENV->funcall(ENV, ENV->intern(ENV, "throw"), 2, args);
 }
 
-#include <stdio.h>
 emacs_value decode_sixel(emacs_env *ENV, ptrdiff_t
 						 NARGS, emacs_value *ARGS, void *DATA)
 {
 		assert(NARGS == 1);
-		
+
 		SIXELSTATUS status = SIXEL_FALSE;
 		sixel_allocator_t *allocator;
 		status = sixel_allocator_new(&allocator, NULL, NULL, NULL, NULL);
-        if (SIXEL_FAILED(status)) {
+		if (SIXEL_FAILED(status)) {
 				panic(ENV, "Failed to create allocator.");
-        }
+		}
 
 		int pwidth, pheight, ncolors;
 		unsigned char *palette;
@@ -44,7 +64,22 @@ emacs_value decode_sixel(emacs_env *ENV, ptrdiff_t
 				panic(ENV, "Failed to retrieve string.");
 		}
 
+		md5(buf, len);
+		for (int i = 0; i < CACHESZ; i++) {
+				if (cache_identifiers[i].h0 == h0 && cache_identifiers[i].h1 == h1 && cache_identifiers[i].h2 == h2 && cache_identifiers[i].h3 == h3) {
+						printf("\nFound cached entry: %p\n", *((void **)cache[i]));
+						return cache[i];
+				}
+		}
+
 		sixel_decode_raw(buf, len, &pixels, &pwidth, &pheight, &palette, &ncolors, allocator);
+
+		printf("Alloc: %p\n", pixels);
+		printf("Hexdump:\n");
+		for (int i = 0; i < pwidth * pheight; i++) {
+				printf("%.2x", pixels[i]);
+		}
+		printf("\n");
 
 		ptrdiff_t output_len = 256 + 12 * (pwidth * pheight);
 		char *output, *cur;
@@ -60,11 +95,53 @@ emacs_value decode_sixel(emacs_env *ENV, ptrdiff_t
 							   *(palette + pixels[i] * 3 + 1),
 							   *(palette + pixels[i] * 3 + 2));
 		}
+		// sixel_allocator_free(allocator, pixels);
 
 		emacs_value ret = ENV->make_string(ENV, (char *) output, strlen(output));
 
+		int sentinel = 0;
+		for (int i = 0; i < CACHESZ; i++) {
+				if (!inuse[i]) {
+						sentinel = 1;
+						cache_identifiers[i].h0 = h0;
+						cache_identifiers[i].h1 = h1;
+						cache_identifiers[i].h2 = h2;
+						cache_identifiers[i].h3 = h3;
+						printf("sizeof: %ld\n", sizeof(struct emacs_value_tag));
+						cache[i] = malloc(sizeof(struct emacs_value_tag));
+						cache[i]->v = *((void **)ret);
+						inuse[i] = 1;
+						break;
+				}
+		}
+		if (!sentinel) {
+				// Evict the whole cache, except for `i`.
+				for (int i = 0; i < CACHESZ; i++) {
+						/* cache_identifiers[j].h0 = 0; */
+						/* cache_identifiers[j].h1 = 0; */
+						/* cache_identifiers[j].h2 = 0; */
+						/* cache_identifiers[j].h3 = 0; */
+						free(cache[i]);
+						inuse[i] = 0;
+				}
+				cache_identifiers[0].h0 = h0;
+				cache_identifiers[0].h1 = h1;
+				cache_identifiers[0].h2 = h2;
+				cache_identifiers[0].h3 = h3;
+				cache[0] = malloc(sizeof(struct emacs_value_tag));
+				cache[0]->v = *((void **)ret);
+				inuse[0] = 1;
+		}
+
+		for (int i = 0; i < CACHESZ; i++) {
+				printf("%.2d: [%c] %p\n", i, inuse[i] ? 'x' : ' ', cache[i]);
+		}
+		printf("\n");
+
 		free(buf);
 		free(output);
+		sixel_allocator_free(allocator, palette);
+		// sixel_allocator_free(allocator, pixels);
 		sixel_allocator_unref(allocator);
 		return ret;
 
@@ -79,10 +156,16 @@ int emacs_module_init(struct emacs_runtime *rt)
 		if (env->size < sizeof (*env))
 				return 2;
 
+		memset(cache_identifiers, '\0', sizeof(cache_identifiers));
+		memset(cache, '\0', sizeof(cache));
+		memset(inuse, '\0', sizeof(inuse));
+
 		emacs_value func = env->make_function(env, 1, 1, decode_sixel, NULL, NULL);
 		emacs_value symbol = env->intern(env, "sixel-decode-string");
 		emacs_value args[] = {symbol, func};
 		env->funcall(env, env->intern (env, "defalias"), 2, args);
+
+		// sizeof(struct emacs_value_tag) = 8
 
 		return 0;
 }
--- pwn/stage0/sixel-el/sixmacs.el
+++ pwn/stage0/sixel-el/sixmacs.el
@@ -1,24 +1,12 @@
 (defvar sixplayground-overlays '())
 
-(defvar sixplayground-test-image
-        "P1
-# Created by GIMP version 2.10.30 PNM plug-in
-32 32
-0000000000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000001100000000000000000
-0000001000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000000000000000000000000
-0000000000000000000000000000000000000000000000000001100000000000100000
-0000000000000100000000000010000000000000000001000000000000100000000000
-0000000100000000000010000000000000000000110000000000100000000000000000
-0000110000000100000000000000000000000011000001000000000000000000000000
-0011111000000000000000000000000000000000000000000000000000000000000000
-00000000000000000000000000000000000000000000")
+(defun sixplayground-make-overlay (start end data-or-function)
+  (let* ((overlay (make-overlay (car match) (cadr match)))
+         (data (if (stringp data-or-function)
+                   data-or-function
+                 (funcall data-or-function))))
+    (overlay-put overlay 'display (create-image data 'pbm t))
+    (push overlay sixplayground-overlays)))
 
 (defun sixplayground-parse ()
   (save-excursion
@@ -27,10 +15,8 @@
       (delete-overlay overlay))
     (setq sixplayground-overlays '())
     (dolist (match (matches-in-buffer "P0;0;0q.*?\\\\"))
-      (let ((overlay (make-overlay (car match) (cadr match))))
-        ;; (overlay-put overlay 'invisible t)
-        (overlay-put overlay 'display (create-image sixplayground-test-image 'pbm t))
-        (push overlay sixplayground-overlays)))))
+      (sixplayground-make-overlay (car match) (cdr match)
+                                  (sixel-decode-string (buffer-substring (car match) (cadr match)))))))
 
 (defun matches-in-buffer (regexp &optional buffer)
   "return a list of matches of REGEXP in BUFFER or the current buffer if not given."
@@ -43,7 +29,7 @@
             (goto-char 1)
             (while (search-forward-regexp regexp nil t 1)
               (push (list (match-beginning 0) (match-end 0)) matches)))))
-      matches)))
+      (reverse matches))))
 
 (defun sixplayground-on-post-command ()
   (when (buffer-modified-p (get-buffer "*scratch*"))
@@ -51,8 +37,6 @@
     (set-buffer-modified-p nil)))
 
 (add-hook 'post-command-hook #'sixplayground-on-post-command)
-(progn (remove-hook 'post-command-hook #'sixplayground-on-post-command)
-       (dolist (overlay sixplayground-overlays)
-         (delete-overlay overlay)))
-
-(string-match "P0;0;0q.*?\\\\" "P0;0;0q\"1;1;32;32#0;2;0;0;0#1;2;100;0;0#1!32~-!32~-!10~|!6~}}!13~$#0!10?A!6?@@-#1!9~Fv!11~F!9~$#0!9?wG!11?w-#1!9~}||zzvv!5nr{!9~$#0!9?@AACCGG!5OKB-#1!32B-\\")
+;; (progn (remove-hook 'post-command-hook #'sixplayground-on-post-command)
+;;        (dolist (overlay sixplayground-overlays)
+;;          (delete-overlay overlay)))

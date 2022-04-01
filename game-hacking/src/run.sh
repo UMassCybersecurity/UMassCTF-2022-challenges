64d711b1784cca11a62a2367875a7a0c8b62f30b
Larry Tseng <larry.tseng.1@gmail.com>
2022-03-31 20:36:45 -0400

    Update run.sh

--- game-hacking/src/run.sh
+++ game-hacking/src/run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-HOSTNAME="${GAME_HOSTNAME:-ws://localhost:8765}"
+HOSTNAME="${GAME_HOSTNAME:-ws://localhost:8124}"
 
 (echo "const HOSTNAME='${HOSTNAME}';"; cat /root/client/game-obfuscated.js) > /root/client/game.js)
 
 pushd /root/client/
-python3 -m http.server 8080 &
+python3 -m http.server 8123 &
 popd
 
 pushd /root/server/
